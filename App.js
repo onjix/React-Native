de67647cbbf974bb9b1a2792c00c93c6b2551b60
jinoson <sojino0813@naver.com>
2023-09-09 15:09:37 +0900

    Study get a my city name

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
+import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { View, Text, Dimensions, StyleSheet, ScrollView } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, Dimensions, StyleSheet, ScrollView} from 'react-native';
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    if(!granted) {
+      setOk(false);
+    }
+    const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5});
+    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps:false});
+    setCity(location[0].city);
+  }
+  useEffect(() => {
+    ask();
+  }, [])
   return (
     <View style={{ flex: 1, backgroundColor: "teal" }}>
       <StatusBar style="light" />
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       {/* pagingEnabled: slides strong  */}
       {/* horizontal: 세로 -> 가로  */}
