53ddb517e8a393dc2b75232e5c640700f78e718a
Jino Son <jinoson@Jinoui-MacBookAir.local>
2023-09-09 13:35:03 +0900

    Study weather first

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {View } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex:1, flexDirection:"row"}}>
-      <View style={{flex:1, backgroundColor:"tomato"}}></View>
-      <View style={{flex:1, backgroundColor:"teal"}}></View>
-      <View style={{flex:1, backgroundColor:"orange"}}></View>
+    <View style={{flex:1, backgroundColor:"teal"}}>
+      <StatusBar style="light"/>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:"teal",
+  },
+  city: {
+    flex:1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName : {
+    fontSize:68,
+    fontWeight: "500",
+  },
+  weather: {
+    flex:3,
+  },
+  day: {
+    flex:1,
+    alignItems:"center",
+  },
+  temp : {
+    fontSize: 178,
+    marginTop:50,
+  },
+  description: {
+    fontSize: 60,
+    marginTop:-30,
+  },
+})
