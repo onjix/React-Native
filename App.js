58cea4d440e5bc1348f922e3301de24f8b4efb00
jinoson <sojino0813@naver.com>
2023-09-10 13:47:32 +0900

    Study add weather

--- App.js
+++ App.js
@@ -1,26 +1,38 @@
 import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
-import { View, Text, Dimensions, StyleSheet, ScrollView} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Dimensions, StyleSheet, ScrollView, ActivityIndicator } from 'react-native';
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const API_KEY = "fb21ab1f6cccc48a029576d8e469a260";
+
 export default function App() {
   const [city, setCity] = useState("Loading...");
-  const [location, setLocation] = useState();
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
-    const {granted} = await Location.requestForegroundPermissionsAsync();
-    if(!granted) {
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
       setOk(false);
     }
-    const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5});
-    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps:false});
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false });
     setCity(location[0].city);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+    const json = await response.json();
+    setDays(
+      json.list.filter((weather) => {
+        if (weather.dt_txt.includes("00:00:00")) {
+          return weather;
+        }
+      })
+    );
   }
   useEffect(() => {
-    ask();
+    getWeather();
   }, [])
+
   return (
     <View style={{ flex: 1, backgroundColor: "teal" }}>
       <StatusBar style="light" />
@@ -33,30 +45,21 @@ export default function App() {
         pagingEnabled //slides strong
         horizontal //세로 -> 가로
         showsHorizontalScrollIndicator={false} //bottom slidebar remove
-        // indicatorStyle='white' //slide bar white
         contentContainerStyle={styles.weather}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="white" size="large" style={{ marginTop: 10 }} />
+          </View>
+        ) : (
+          days.map((day, index) =>
+            <View key={index} style={styles.day}>
+              <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+              {/* <Text style={styles.description}>{day.weather[0].main}</Text> */}
+            </View>)
+        )}
       </ScrollView>
-
     </View>
   );
 }
@@ -82,11 +85,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   temp: {
-    fontSize: 178,
-    marginTop: 50,
+    fontSize: 130,
+    marginTop: 40,
   },
   description: {
     fontSize: 60,
-    marginTop: -30,
+    marginTop: -10,
+  },
+  tinyText: {
+    fontSize:20,
   },
 })
